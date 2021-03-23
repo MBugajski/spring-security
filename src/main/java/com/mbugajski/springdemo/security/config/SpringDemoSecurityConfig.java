eb57abc319ebd13bee0005c64421867dc2207cd4
MBugajski <michalbugajski@outlook.com>
2021-03-23 02:12:02 +0100

    Added inMemoryAuthentication for the time being

--- src/main/java/com/mbugajski/springdemo/security/config/SpringDemoSecurityConfig.java
+++ src/main/java/com/mbugajski/springdemo/security/config/SpringDemoSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +14,13 @@ public class SpringDemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("john").password("j123").roles("EMPLOYEE"))
+		.withUser(users.username("mike").password("m123").roles("MANAGER"))
+		.withUser(users.username("tom").password("t123").roles("ADMIN"));
+		
 	}
 
 	
