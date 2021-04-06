1c61bb8911a875b9221e52ef0fc9433922775816
MBugajski <michalbugajski@outlook.com>
2021-04-06 23:43:37 +0200

    Added local CSS file and configured Spring Security  login form to use it.

--- src/main/java/com/mbugajski/springdemo/security/config/SpringDemoSecurityConfig.java
+++ src/main/java/com/mbugajski/springdemo/security/config/SpringDemoSecurityConfig.java
@@ -27,6 +27,7 @@ public class SpringDemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
+			.antMatchers("/css/**").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
