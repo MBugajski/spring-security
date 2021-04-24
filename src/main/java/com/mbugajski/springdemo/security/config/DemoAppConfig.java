719d86c4e9f88b75da0102c257cd933ed4dc1ff2
MBugajski <michalbugajski@outlook.com>
2021-04-24 23:49:35 +0200

    Defined @PropertySource

--- src/main/java/com/mbugajski/springdemo/security/config/DemoAppConfig.java
+++ src/main/java/com/mbugajski/springdemo/security/config/DemoAppConfig.java
@@ -3,6 +3,7 @@ package com.mbugajski.springdemo.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="package com.mbugajski.springdemo.security")
+@PropertySource({ "classpath:persistence-mysql.properties" })
 public class DemoAppConfig {
 
 	// define a bean for ViewResolver
