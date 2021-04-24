d00de9c43dcfb188112640924d9bf1a30efb98b2
MBugajski <michalbugajski@outlook.com>
2021-04-24 23:51:28 +0200

    Defined environment

--- src/main/java/com/mbugajski/springdemo/security/config/DemoAppConfig.java
+++ src/main/java/com/mbugajski/springdemo/security/config/DemoAppConfig.java
@@ -1,20 +1,29 @@
 package com.mbugajski.springdemo.security.config;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="package com.mbugajski.springdemo.security")
 @PropertySource({ "classpath:persistence-mysql.properties" })
-public class DemoAppConfig {
+public class DemoAppConfig implements WebMvcConfigurer {
 
-	// define a bean for ViewResolver
+    @Autowired
+    private Environment env;
+    
+    private Logger logger = Logger.getLogger(getClass().getName());
+
 
 	@Bean
 	public ViewResolver viewResolver() {
