145f95eb131b7a910ea5b829e39d701eb01696d6
MBugajski <michalbugajski@outlook.com>
2021-04-27 23:51:18 +0200

    Added DataSource to WebSecurity config

--- src/main/java/com/mbugajski/springdemo/security/config/SpringDemoSecurityConfig.java
+++ src/main/java/com/mbugajski/springdemo/security/config/SpringDemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.mbugajski.springdemo.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +15,9 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SpringDemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
