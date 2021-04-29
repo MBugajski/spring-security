c8fce2490ec21b3d8e9412e7f84e9e2499992fa1
MBugajski <michalbugajski@outlook.com>
2021-04-29 23:50:10 +0200

    Fixed wrong DataSource import

--- src/main/java/com/mbugajski/springdemo/security/config/DemoAppConfig.java
+++ src/main/java/com/mbugajski/springdemo/security/config/DemoAppConfig.java
@@ -3,7 +3,7 @@ package com.mbugajski.springdemo.security.config;
 import java.beans.PropertyVetoException;
 import java.util.logging.Logger;
 
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
